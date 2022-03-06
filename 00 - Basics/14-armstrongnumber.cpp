b858f0e360af8b4026c03ac2a4b67cb39f04c6c9
sarvaniballamudi <75388075+sarvaniballamudi@users.noreply.github.com>
2022-03-06 16:28:39 +0530

    Update 14-armstrongnumber.cpp

--- 00 - Basics/14-armstrongnumber.cpp
+++ 00 - Basics/14-armstrongnumber.cpp
@@ -5,19 +5,22 @@ int main() {
 	int n;
 	cin>>n;
 
-	int sum=0,num=n;
+	int sum=0,num=n,order=0;
+	//Finding order of number
+	while(num>0) {
+		order++;
+		num/=10;
+	}
+	num=n;
 	while(num>0) {
 		int ld = num%10;
-		sum+=round(pow(ld,3));
+		sum+=round(pow(ld,order));
 		num/=10;
 	}
 
 	if(n == sum)
-		cout<<"Armstrong.";
+		cout<<"Armstrong."<<endl;
 	else
-		cout<<"Not Armstrong.";
+		cout<<"Not Armstrong."<<endl;
 	return 0;
 }
-
-//Not Complete Solution.
-//To be implemented from GFG
